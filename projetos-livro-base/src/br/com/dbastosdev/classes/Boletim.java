4c00c19e0325e967ba7d8f6bf0f3c4272d151d97
dbastosdev <dbastos.dev@gmail.com>
2022-09-16 18:13:16 -0300

    cap. 3 finalizado. Polimorfismo

--- projetos-livro-base/src/br/com/dbastosdev/classes/Boletim.java
+++ projetos-livro-base/src/br/com/dbastosdev/classes/Boletim.java
@@ -29,10 +29,34 @@ public class Boletim {
 		
 	}
 	
-	public void calcular_media() {
-		
+	/*
+	 * Usando polimorfismo estático (sobrecarga) para o cálculo de média
+	 * 
+	 * GRADUAÇÃO:
+	 * 
+	 * AM = Avaliação Multidisciplinar (e vale 30% da média)
+	 * AC = Avaliação Continuada (e vale 20% da média)
+	 * AS = Avaliação Semestral (e vale 50% da média)
+	   MS = Média Semestral
+	   
+	   PÓS-GRADUAÇÃO:
+	   
+	   PM = Projeto Multidisciplinar (e vale 40% da média)
+	   AS = Avaliação Semestral (e vale 60% da média)
+	   MM = Média Modular
+	   
+     */
+	
+	// Cálculo da graduação
+	public double calcular_media(double AM, double AC, double AS) {
+		return  (AM * 0.3) + (0.2 * AC) + (0.5 * AS); 
 	}
 	
-	
-	
+	// Cálculo da graduação
+	public double calcular_media(double PM, double AS) {
+		return (PM * 0.4) + (AS * 0.6); 
+	}
+
+
+
 }
---
+++ projetos-livro-base/src/br/com/dbastosdev/test/SwingPolimorfismo.java
@@ -0,0 +1,23 @@
+package br.com.dbastosdev.test;
+
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+
+import br.com.dbastosdev.classes.Boletim;
+
+public class SwingPolimorfismo {
+
+	public static void main(String[] args) {
+		
+		// Objetos boletim
+		Boletim b1 = new Boletim();
+		Boletim b2 = new Boletim();
+		
+		double m_grad = b1.calcular_media(9.5, 4.7, 6.9);
+		double m_pos = b2.calcular_media(7.4, 9.2);
+		
+	    JOptionPane.showMessageDialog(null, "A média do aluno de graduação é " + m_grad + "\nA média do aluno de pós-graduação é " + m_pos, "BOLETIM", JOptionPane.PLAIN_MESSAGE);
+
+	}
+
+}
